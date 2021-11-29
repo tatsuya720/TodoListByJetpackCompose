688fca92f2006f04d5e240745d82b7c7e2b2209c
tatsuya.nakajima <>
2021-11-29 10:47:00 +0900

    未完了リストアイテムUI追加

--- app/src/main/java/com/example/todolistbyjetpackcompose/ui/notDoneList/NotDoneListItem.kt
+++ app/src/main/java/com/example/todolistbyjetpackcompose/ui/notDoneList/NotDoneListItem.kt
@@ -1,2 +1,9 @@
 package com.example.todolistbyjetpackcompose.ui.notDoneList
 
+import androidx.compose.runtime.Composable
+import com.example.todolistbyjetpackcompose.model.TodoItem
+
+@Composable
+fun NotDoneListItem(todoItem: TodoItem) {
+
+}
