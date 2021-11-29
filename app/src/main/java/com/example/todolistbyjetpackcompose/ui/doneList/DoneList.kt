476f667dc5f350efab07306d5834b53f709e1567
tatsuya.nakajima <>
2021-11-29 16:29:01 +0900

    未完了一覧と見た目を同じようにする

--- app/src/main/java/com/example/todolistbyjetpackcompose/ui/doneList/DoneList.kt
+++ app/src/main/java/com/example/todolistbyjetpackcompose/ui/doneList/DoneList.kt
@@ -3,6 +3,7 @@ package com.example.todolistbyjetpackcompose.ui.doneList
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.ExperimentalMaterialApi
+import androidx.compose.material.Scaffold
 import androidx.compose.runtime.Composable
 import com.example.todolistbyjetpackcompose.model.TodoItem
 import com.example.todolistbyjetpackcompose.model.TodoState
@@ -12,10 +13,16 @@ import com.example.todolistbyjetpackcompose.ui.listItem.TodoListItem
 @ExperimentalMaterialApi
 @Composable
 fun DoneList(itemList: List<TodoItem>) {
-    LazyColumn {
-        items(itemList) { item ->
-            //NotDoneListItem(title = item.title, description = item.description)
-            TodoListItem(title = item.title, description = item.description, state = TodoState.Done)
+    Scaffold {
+        LazyColumn {
+            items(itemList) { item ->
+                //NotDoneListItem(title = item.title, description = item.description)
+                TodoListItem(
+                    title = item.title,
+                    description = item.description,
+                    state = TodoState.Done
+                )
+            }
         }
     }
 }
